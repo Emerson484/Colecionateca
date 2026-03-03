8ccbc15d828fddd34107e4d53479368a7f12242d
Emerson Jesus <emersomjesus999@gmail.com>
2026-03-03 15:08:50 -0300

    Create TelaInicialProjetoBibliotecaController.java
    criar TelaInicialProjetoBibliotecaController

---
+++ src/data/controller/TelaInicialProjetoBibliotecaController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class TelaInicialProjetoBibliotecaController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private Button BotaoLivros;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+   
+    @FXML
+    public void OnActionBotaoLivros(ActionEvent event) throws IOException,RuntimeException{
+        Parent root = FXMLLoader.load (getClass().getResource("/fxml/TelaLivros.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
